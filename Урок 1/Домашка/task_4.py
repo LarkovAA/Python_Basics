322e0cc73202f17f5f3ccf3d65ee5caccc35d59b
kane93 <lexlar@mail.ru>
2021-01-15 19:43:41 +0400

    Выполнил 4 задание что бы можно было искать цифру с любого внесенного числа.

--- Урок 1/Домашка/task_4.py
+++ Урок 1/Домашка/task_4.py
@@ -1,18 +1,45 @@
-max = None
+max = 0
+number = int(input('Введите любое положительое число '))
 while True:
-    number = int(input('Введите любое положительое число больше 10 но меньше 1000? '))
-    if number >= 10 and number < 1000:
-        hundred_num = number // 100 # вычисляем сколько целых сотен есть в числе
-        dozen_num = number % 100 / 10  # вычисляем сколько целых десятков есть в числе
-        number = number % 10 # вычисляеть оставшуюся единицу числа
+    if number >= 1000:
+        number = number // 1000
+        num_min = number % 1000
+        if num_min < 1000:
+            hundred_num = int(num_min // 100)  # вычисляем сколько целых сотен есть в числе
+            dozen_num = int(num_min % 100 / 10)  # вычисляем сколько целых десятков есть в числе
+            sungle_num = int(num_min % 10)  # вычисляеть оставшуюся единицу числа
         # делаем проверку какое число единичное или десятка больше и записывем в переменную max
-        if number >= dozen_num:
-            max = number
+            if max >= sungle_num:
+                max = max
+            else:
+                max = sungle_num
+            if max >= dozen_num:
+                max = max
+            else:
+                max = dozen_num
+            if max >= hundred_num:
+                max = max
+            else:
+                max = hundred_num
+
+    else:
+        hundred_num = int(number // 100)  # вычисляем сколько целых сотен есть в числе
+        dozen_num = int(number % 100 / 10)  # вычисляем сколько целых десятков есть в числе
+        sungle_num = int(number % 10)  # вычисляеть оставшуюся единицу числа
+        # делаем проверку какое число единичное или десятка больше и записывем в переменную max
+        if max >= sungle_num:
+            max = max
+        else:
+            max = sungle_num
+        if max >= dozen_num:
+            max = max
         else:
             max = dozen_num
         if max >= hundred_num:
             max = max
         else:
             max = hundred_num
-        print(f'Самая большая цифра из вашего числа {max}')
         break
+print(f'Максимальная цифра вашего числа {max}')
+
+
