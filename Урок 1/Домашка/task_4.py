56a963186936dc3f134e74393dee73b2abe5bb11
kane93 <lexlar@mail.ru>
2021-01-16 20:53:39 +0400

    Не работает поиск если цифра с лева самое большое.

--- Урок 1/Домашка/task_4.py
+++ Урок 1/Домашка/task_4.py
@@ -1,4 +1,5 @@
 max = 0
+stop_cicle = False
 number = int(input('Введите любое положительое число '))
 # если введенное чисо меньше десяти то мы прибавляем к этому значению 10
 if number >= 1 and number <= 9:
@@ -12,4 +13,9 @@ while number // 10 !=0:
     else:
         max = max
     number = number // 10
+    if number >= 1 and number <= 9:
+        number = number + 10
+        stop_cicle = True
+    if stop_cicle == True:
+        break
 print(f'Самая большая цифра в вашем числе это {max}')
