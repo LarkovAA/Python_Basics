6350970b632c8e833dd729f777bec4397fb9623f
kane93 <lexlar@mail.ru>
2021-01-20 22:10:42 +0400

    Оптимизировал 6-е задание.

--- Урок 2/Домашнее задание/task_6.py
+++ Урок 2/Домашнее задание/task_6.py
@@ -2,16 +2,26 @@ print('Заполните информацию о товаре')
 item_number = None
 product = [] # создаю список всех продуктов
 ex_t = None # создаю переменную для выхода из цикла
+list_name, list_price, list_quantity, list_unit_measurement = [], [], [], []
 # создал цикл в котором делаю запрос на добавления значений в словать и нумерацию. НА каждом цикле добавляю новый элемент нмера и словаря.
 while True:
+    # создаем словарь с характеристиками продукта
     product_characteristics = {}
+    # создаем словарь для аналиа продуктов где значения это списки которые будут записываться при заполнении элементов словарей значениями
+    product_analytics = {'название': list_name, 'цена': list_price, 'количество': list_quantity,
+                         'ед-ца измерения': list_unit_measurement}
+    # даю запрос на заполнения онформацией о товаре а так же жобавляю эти значения в соответствующие списки.
     item_number = input('Введите номер товара ? ')
     product_characteristics['название'] = input('Введите название товара ?  ')
+    list_name.append(product_characteristics['название'])
     product_characteristics['цена'] = input('Введите цену товара ?  ')
+    list_price.append(product_characteristics['цена'])
     product_characteristics['количество'] = input('Введите количество товара ?  ')
+    list_quantity.append(product_characteristics['количество'])
     product_characteristics['ед-ца измерения'] = input('Введите единицу измерения товара ?  ')
-
+    list_unit_measurement.append(product_characteristics['ед-ца измерения'])
     product.append((item_number, product_characteristics))
+
     # делаю запрос на выход из цикла
     ex_t = input('Вы ввели данные по товару. Хотите продолжить залолнение ?(Да или Нет) ')
     ex_t = ex_t.lower()
@@ -19,24 +29,4 @@ while True:
         break
 
 print(f'Вы ввели следующие данные {product}')
-# создал новый словарь где будет записаны значения всех ключей в созданых элементов словаря product
-product_analytics = {'название': None, 'цена': None, 'количество': None, 'ед-ца измерения': None}
-# создал списки в которых буду записывать все значения во всех созданых элементов по ключам
-list_name, list_price, list_quantity, list_unit_measurement = [], [], [], []
-# объеденил все созданные списки в один
-list_product_characteristics = [list_name, list_price, list_quantity, list_unit_measurement]
-# создал список всех ключей в списке product
-list_prod_analy = ['название', 'цена', 'количество', 'ед-ца измерения']
-# создал цикл который перебирает все элементы кортежа созданые в списке product
-for el in product:
-    num = 0
-    for num_list in list_product_characteristics: # перебираем созданный вложеный список пустых списков
-        num_list.append(el[1].get(list_prod_analy[num])) # добавляем в каждый пустой список значения какого то одного ключай
-        num += 1
-    # в новый словарь добавляем все значения который имеет каждый ключь во всех элементах соваря в списке product
-    product_analytics['название'] = list_name
-    product_analytics['цена'] = list_price
-    product_analytics['количество'] = list_quantity
-    product_analytics['ед-ца измерения'] = list_unit_measurement
-
 print(f'Выводим аналитическую информацию {product_analytics}')
