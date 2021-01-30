2409926c920633b98266052c227e663dc6510e48
kane93 <lexlar@mail.ru>
2021-01-30 13:06:55 +0400

    добавил коментарии к 6 заданию

--- Урок 5/домашнее задание/task_6.py
+++ Урок 5/домашнее задание/task_6.py
@@ -1,29 +1,39 @@
 with open('file_task_6.txt', 'r', encoding='UTF-8') as file_t_6:
+    # открываем файл на чтение и записываем в список данные этого файла
     readlines_file_t_6 = file_t_6.readlines()
     list_file_t_6 = []
 
+    # проходим по каждому элементу нового списка и делин его по запятой
     for file_str in readlines_file_t_6:
         list_file_t_6.append(file_str.split(','))
+    # создаем два списка в которых будем содержать элементы ключей и значений
     key_dict = []
     value_dict = []
-
+    # дабавляем путем деление строк на : до знака : все элементы мы передаем в ключи а после в значения
     for list_el in list_file_t_6:
         for str_el in list_el:
             key_dict.append(str_el[: str_el.index(':')])
             value_dict.append(str_el[str_el.index(':') + 1:])
+    # сам спикок со значениями мы разбиваем по пробелу
     value_dict = list(map(lambda x: x.split(' '), value_dict))
 
+    # создаем новый список со значениями в который мы запишем строки которые могут перевестись в тип данных число
     new_value_dict = []
     for el in value_dict:
+        # записываем в новый словарь значений список который будет содержать элементы которые могут изменить значение на число
+        # если взникает ошибка то мы ее пропускаем
         try:
             new_value_dict.append(copy_num_el)
         except:
             pass
+        # создан словарь который и будет содержать строки которые могут перевестись в числа
         copy_num_el = []
         for num_el in el:
-            if num_el == '' or num_el == '-\n':
+            # если элемент содержит пустую строку или просто - или со знком преноса то мы пропускаем данный элемент
+            if num_el == '' or num_el == '-\n' or num_el == '-':
                 continue
             else:
+                # создаем переменную в которой будем из каждого элемента записывать строки имеющие тип значния числа
                 number = ''
                 for int_el in num_el:
                     try:
@@ -31,59 +41,17 @@ with open('file_task_6.txt', 'r', encoding='UTF-8') as file_t_6:
                             number = number + int_el
                     except:
                         continue
+                # записываем данную переменную в список
                 copy_num_el.append(number)
-
+                # если мы рассматриваем послежний элемент в списке value_dict то записываем данные сразуже после заполнения списка copy)num_el
+                if len(value_dict) - 1 == value_dict.index(el):
+                    new_value_dict.append(copy_num_el)
+    # все элементы нового списка значений мы меняем тип на числа
     new_value_dict = list(map(lambda x: list(map(lambda y: int(y), x)), new_value_dict))
-
+    # создаем словарь и записываем в него новые элементы
+    print(len(key_dict))
     dara_items = {}
-    for num in range(0, len(key_dict) - 1):
+    for num in range(0, len(key_dict)):
         dara_items[key_dict[num]] = sum(new_value_dict[num])
 
     print(f'По итогу каждый предмет имеет следующее колличество часов на обучение {dara_items}')
-
-
-
-
-
-
-
-
-'''
-                       for int_el in num_el:
-                try:
-                    if type(int(int_el)) == int:
-                        num = num + int_el
-                        value_dict[value_dict.index(el)][el.index(num_el)] = num
-                except:
-                    continue
-    print(value_dict)
-           '''
-
-'''
-    list_value_dict = list(map(lambda x: x.split(' '), value_dict))
-    print(list_value_dict)
-
-    copy_list_value_dict =  list_value_dict.copy()
-
-    for el in list_value_dict:
-        #print(f'1 уровень {el}')
-        list_el = []
-
-        for str_el in el:
-            list_str_el = []
-            #print(f'2 уровень {str_el}')
-            for int_el in str_el:
-                if int_el == '(':
-                    list_str_el = []
-                #print(f'3 уровень {int_el}')
-                try:
-                    if type(int(int_el)) == int:
-                        number = int_el
-                        list_str_el.append(number)
-                        list_el.append(list_str_el)
-                        list_str_el = []
-                        list_value_dict[list_value_dict.index(el)] = list_el
-                except:
-                    continue
-
-   '''
