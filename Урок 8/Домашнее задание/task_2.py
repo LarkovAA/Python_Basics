59ebcb984f025f9b365a493ecf827eae680b657e
kane93 <lexlar@mail.ru>
2021-02-11 16:30:19 +0400

    сделал 2 задание

--- Урок 8/Домашнее задание/task_2.py
+++ Урок 8/Домашнее задание/task_2.py
@@ -1,1 +1,17 @@
 class Division_0():
+    def __str__(self):
+        return print(f'Вы делите на 0. Так нельзя.')
+
+num_1 = int(input('Ведите первое число ? '))
+num_2 = int(input('Ведите второе число ? '))
+
+try:
+    rezult = num_1 / num_2
+except:
+    print(Division_0())
+finally:
+    if num_2 == 0:
+        print(f'Ответ {num_1}')
+    else:
+        print(f'Ответ {rezult}')
+
