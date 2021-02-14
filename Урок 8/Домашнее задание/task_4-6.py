914d36246e976bc6ab5ed46f45492f787bfdd4f9
kane93 <lexlar@mail.ru>
2021-02-14 18:34:52 +0400

    Сделал задание 6/

--- Урок 8/Домашнее задание/task_4-6.py
+++ Урок 8/Домашнее задание/task_4-6.py
@@ -1,9 +1,15 @@
+import random
+# создаем класс склад орг техники
 class Office_equipment_warehouse():
+    # создаем списки в которые будем записывать созданные объекты орг техники и их колличество какое было переведено на склад или в подразделения
     warehouse = []
     division = []
+    list_id = []
 
+    # создем метод класса в который будем записывать какую орг технику мы отправили на склад
     @staticmethod
     def admission_warehouse(office_equipment, quantity):
+        # проверяем не являеться ли введенное значение строкой
         try:
             Office_equipment_warehouse.chekink_int(quantity)
         except:
@@ -13,6 +19,7 @@ class Office_equipment_warehouse():
             dic_admis_wareh[office_equipment] = quantity
             Office_equipment_warehouse.warehouse.append(dic_admis_wareh)
 
+    # создаем метод класса с помошью которого мы переводим технику со склада в отделы.
     @staticmethod
     def transfer_divisions(company_division, quantity, office_equipment):
         try:
@@ -20,12 +27,17 @@ class Office_equipment_warehouse():
         except:
             return print('Колличество техники вы ввели не числом а строкой')
         else:
+            #  спомошью цикла фор мы прверяем элементы списка warehouse
             for el in Office_equipment_warehouse.warehouse:
+                # создаем переменную в которую записываем ключ по каждому циклу
                 keys_wareh = tuple(el.keys())[0]
+                # если ключ имеет одинаковое значение со значение той орг техники которую хотим перевести
                 if keys_wareh == office_equipment:
+                    # ищим индек этого элемента в списке
                     in_x = Office_equipment_warehouse.warehouse.index(el)
+                    # меняем значение словаря из списка склад уменьшаем на то количество техники которую переводим в другой отдел
                     Office_equipment_warehouse.warehouse[in_x][office_equipment] = Office_equipment_warehouse.warehouse[in_x][office_equipment] - quantity
-
+            # создаем словарь в оторый будет значение для другого словаря который будет отражать в какой отдел какую технику и сколько перенесли
             add_division = {}
             add_division[office_equipment] = quantity
 
@@ -41,79 +53,78 @@ class Office_equipment_warehouse():
         else:
             raise TypeError
 
-    def __str__(self):
-        return f'{Office_equipment_warehouse.warehouse}'
-
 class Office_equipment():
     quantity_office_equipment = 0
 
-    def __init__(self, stamp, model, price, id):
+    def __init__(self, stamp, model, price):
         self.stamo = stamp
         self.model = model
         self.price = price
-        self.id = id
-
-    @classmethod
-    def __str__(cls):
-        return cls.__init__()
+        while True:
+            self.id = int(random.randint(1000, 9999))
+            if self.id not in Office_equipment_warehouse.list_id:
+                Office_equipment_warehouse.list_id.append(self.id)
+                break
 
 class Printer(Office_equipment):
     Office_equipment.quantity_office_equipment += 1
     # в данный класс мы записываем следующие атрибуты марка модель цена йди, марка красок, скорость печати
-    def __init__(self, stamp, model, price, id, paint_brand, paint_speed):
-        self.stamp = stamp
-        self.model = model
-        self.price = price
-        self.id = id
-        self.paint_brand = paint_brand
-        self.paint_speed = paint_speed
-
-        self.tuple_atribut_object = (self.stamp, self.model, self.price, self.id, self.paint_brand, self.paint_speed)
-
-    def __str__(self):
-        return {self.stamp}, {self.model}, {self.price}, {self.id}, {self.paint_brand}, {self.paint_speed}
+    def __init__(self, stamp, model, price, paint_brand, paint_speed):
+            self.price = price
+            self.paint_speed = paint_speed
+            self.stamp = stamp
+            self.model = model
+            self.paint_brand = paint_brand
+            while True:
+                self.id = int(random.randint(1000, 9999))
+                if self.id not in Office_equipment_warehouse.list_id:
+                    Office_equipment_warehouse.list_id.append(self.id)
+                    break
+            self.tuple_atribut_object = (self.stamp, self.model, self.price, self.id, self.paint_brand, self.paint_speed)
 
 
 class Scaner(Office_equipment):
     Office_equipment.quantity_office_equipment += 1
     # в данном класе записываем марку, модель, цена, йди, скорость сканирования.
-    def __init__(self, stamp, model, price, id, scaling):
+    def __init__(self, stamp, model, price, scaling):
         self.stamp = stamp
         self.model = model
         self.price = price
-        self.id = id
         self.scaling = scaling
-
+        while True:
+            self.id = int(random.randint(1000, 9999))
+            if self.id not in Office_equipment_warehouse.list_id:
+                Office_equipment_warehouse.list_id.append(self.id)
+                break
         self.tuple_atribut_object = (self.stamp, self.model, self.price, self.id, self.scaling)
 
-    def __str__(self):
-        return {self.stamp}, {self.model}, {self.price}, {self.id}, {self.scaling}
 
 class Copy_shop(Office_equipment):
     Office_equipment.quantity_office_equipment += 1
 
     # в данный класс мы записываем следующие атрибуты марка модель цена йди, марка красок, скорость печати, скорость сканирования
-    def __init__(self, stamp, model, price, id, paint_brand, paint_speed, scaling):
+    def __init__(self, stamp, model, price, paint_brand, paint_speed, scaling):
         self.stamp = stamp
         self.model = model
         self.price = price
-        self.id = id
         self.paint_brand = paint_brand
         self.paint_speed = paint_speed
         self.scaling = scaling
+        self.id = None
+        while True:
+            self.id = int(random.randint(1000, 9999))
+            if self.id not in Office_equipment_warehouse.list_id:
+                Office_equipment_warehouse.list_id.append(self.id)
+                break
 
         self.tuple_atribut_object = (self.stamp, self.model, self.price, self.id, self.paint_brand, self.paint_speed,
                                self.scaling)
 
-    def __str__(self):
-        return {self.stamp}, {self.model}, {self.price}, {self.id}, {self.paint_brand}, {self.paint_speed}, {self.scaling}
-
-
-print_1 = Printer('Принтер samsung', 'A-320', 2500, 1, 'палитра', 1000)
+print_1 = Printer('Принтер samsung', 'A-320', 2500, 'палитра', 1000)
 #print(print_1)
-scaner_1 = Scaner('Сканер samsung', 'A-100', 1500, 1, 5)
+scaner_1 = Scaner('Сканер samsung', 'A-100', 1500, 5)
 #print(scaner_1)
-copy_shope_1 = Copy_shop('Ксерокс samsung', 'S2000', 8000, 1, 'palit_sam', 300, 3)
+copy_shope_1 = Copy_shop('Ксерокс samsung', 'S2000', 8000, 'palit_sam', 300, 3)
 #print(copy_shope_1)
 
 print(Office_equipment.quantity_office_equipment)
@@ -124,10 +135,12 @@ Office_equipment_warehouse.admission_warehouse(copy_shope_1.tuple_atribut_object
 
 print(Office_equipment_warehouse.warehouse)
 
-#Office_equipment_warehouse.transfer_divisions('финансы', 1, copy_shope_1.tuple_atribut_object)
-Office_equipment_warehouse.transfer_divisions('бухгалтерия', 'три', scaner_1.tuple_atribut_object)
-#Office_equipment_warehouse.transfer_divisions('обучения персонала', 4, print_1.tuple_atribut_object)
+Office_equipment_warehouse.transfer_divisions('финансы', 1, copy_shope_1.tuple_atribut_object)
+Office_equipment_warehouse.transfer_divisions('бухгалтерия', 3, scaner_1.tuple_atribut_object)
+Office_equipment_warehouse.transfer_divisions('обучения персонала', 4, print_1.tuple_atribut_object)
 
 print(Office_equipment_warehouse.division)
 
 print(Office_equipment_warehouse.warehouse)
+
+print(Office_equipment_warehouse.list_id)
